6d1eeb40bd65f174c4a0439d9f1ffd45cc0dfd6c
Arian Pashae <empoyarian@gmail.com>
2022-08-16 18:20:41 +0430

    Solutech Wordpress Theme

---
+++ theme/solutech/pixtheme/pixinit.php
@@ -0,0 +1,17 @@
+<?php
+
+    /* Define library path */
+
+    /* Load ADMIN main file */
+    require_once(get_template_directory() . '/pixtheme/admin/index.php');
+
+    /* Load FUNCTIONS main file */
+    require_once(get_template_directory() . '/pixtheme/functions/index.php');
+
+    /* Load VC MAP files */
+    require_once(get_template_directory() . '/vc_templates/vc_maps/index.php');
+
+    /* Load Plugins */
+    require_once(get_template_directory() . '/pixtheme/install/index.php');
+
+?>
